e990706e827d235dd6689e0550d8b39ad0bdabaf
Scott Kellum <scott@scottkellum.com>
2012-08-15 09:35:11 -0400

    cleanup

--- lib/color-schemer.rb
+++ lib/color-schemer.rb
@@ -1,9 +1,9 @@
 require 'compass'
 Compass::Frameworks.register("color-schemer", :path => "#{File.dirname(__FILE__)}/..")
 
-module ModularScale
+module ColorSchemer
   
-  VERSION = "0.1.0"
+  VERSION = "0.1.3"
   DATE = "2011-09-15"
 
 end
