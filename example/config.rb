06ccaf0a50a1729d40c4720399e9b4035f11b64a
Scott Kellum <scott@scottkellum.com>
2012-11-24 13:29:18 -0500

    Some cleanup

---
+++ example/config.rb
@@ -0,0 +1,5 @@
+# Set this to the root of your project when deployed:
+add_import_path '../stylesheets'
+http_path = 'html'
+css_dir = 'css'
+sass_dir = 'sass'
--- test/config.rb
+++
@@ -1,7 +0,0 @@
-# Require any additional compass plugins here.
-require 'color-schemer'
-
-# Set this to the root of your project when deployed:
-http_path = 'html'
-css_dir = 'html/css'
-sass_dir = 'sass'
